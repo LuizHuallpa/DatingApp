a72a5a912955098c285773411b14b2e27a0d5e47
Luiz Felipe Vicente Nina Huallpa <luiz.huallpa@gmail.com>
2023-01-13 21:31:33 -0300

    Setting main photo is complete client side

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -5,27 +5,26 @@ import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getMembers() {
     if (this.members.length > 0) return of(this.members);
     return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map(members => {
+      map((members) => {
         this.members = members;
         return members;
       })
-
     );
   }
 
   getMember(username: string) {
-    const member = this.members.find(x => x.userName === username);
-    if(member) return of(member)
+    const member = this.members.find((x) => x.userName === username);
+    if (member) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
@@ -33,8 +32,12 @@ export class MembersService {
     return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
         const index = this.members.indexOf(member);
-        this.members[index] = {...this.members[index], ...member}
+        this.members[index] = { ...this.members[index], ...member };
       })
     );
-}
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {})
+  }
 }
