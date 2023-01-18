ea98b87be955532631ed9c8db674162ce21d64d0
Luiz Felipe Vicente Nina Huallpa <luiz.huallpa@gmail.com>
2023-01-18 19:00:09 -0300

    Pagination added and tested in Postman

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using AutoMapper;
@@ -28,10 +29,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        public async Task<ActionResult<PagedList<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync();
+            var users = await _userRepository.GetMembersAsync(userParams);
 
+            Response.AddPaginationHeader(new PaginationHeader(users.CurrentPaged,
+                                                             users.PageSize,
+                                                             users.TotalCount,
+                                                             users.TotalPages));
 
             return Ok(users);
         }
