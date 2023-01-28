66e2b2c8f8e554e4679fc2b755021065e2609355
Luiz Felipe Vicente Nina Huallpa <luiz.huallpa@gmail.com>
2023-01-28 14:14:12 -0300

    added roles in API

--- API/Program.cs
+++ API/Program.cs
@@ -34,13 +34,17 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
-try{
+try
+{
     var context = services.GetRequiredService<DataContext>();
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedUsers(userManager);
+    await Seed.SeedUsers(userManager, roleManager);
 
-}catch(Exception ex){
+}
+catch (Exception ex)
+{
 
     var logger = services.GetService<ILogger<Program>>();
     logger.LogError(ex, "An error occurred during migration");
